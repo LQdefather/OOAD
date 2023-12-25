fa084b9de48275f41f90828589696bc451192551
ryantang <ryantang_register@hotmail.com>
2023-12-25 12:25:02 +0800

    Experiment login page 2

--- pages/login.vue
+++ pages/login.vue
@@ -65,7 +65,7 @@ export default {
         .then(response => {
           // 处理响应
 
-          this.$cookies.set('sessionid', response.data.sessionid, '7d','/','www.susdorm.online',true,null);
+          this.$cookies.set('sessionid', response.data.sessionid, '7d',null,null,true,null);
           this.$router.replace('/person');
         })
         .catch(error => {
